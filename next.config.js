31a05cff9c7ef0246f5c49f2c2e295a0b93eba01
LNam14 <lehoainam2506@gmail.com>
2024-07-12 12:40:24 +0700

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const TerserPlugin = require("terser-webpack-plugin")
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack(config) {
+        // Remove TerserPlugin if not needed for minification
+        if (!config.optimization) {
+            config.optimization = {}
+        }
+        if (!config.optimization.minimizer) {
+            config.optimization.minimizer = []
+        }
+
+        // Add TerserPlugin with optimized options
+        config.optimization.minimizer.push(
+            new TerserPlugin({
+                terserOptions: {
+                    mangle: false,
+                },
+                // Limit parallelism to reduce memory usage
+                parallel: false,
+            })
+        )
+
+        config.optimization.minimize = false
+
+        return config
+    },
+}
 module.exports = nextConfig
